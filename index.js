21f8d5f3e9a4e4d357bfe0388a63e64351b30420
Nicolas Martinez Gordillo <1143876267@academico.icesi.edu.co>
2018-11-16 08:52:00 -0500

    se cambian los estilos, y se declaran las dependencias

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-window.addEventListener('load', function(){
-    
-})
+const express = require('express');
+
+    var app = express();
+
+    const engines = require('consolidate');
+    const handlebars = require('handlebars');
+    const bodyParser = require('body-parser');
+    const hbs = require('express-handlebars');
+
+
+    app.engine('hbs', engines.handlebars);
+    app.set('views', './views');
+    app.set('view engine', 'hbs');
+    app.use(express.static('public'));
+
+
+    app.get('/titulo', function(request, response){
+        res.render('titulo');
+    })
+
+
+    app.listen(2000);
